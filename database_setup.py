0a3dbb23063c025265a8c6a0a9419bdb6dc601fb
rjl-8 <34580339+rjl-8@users.noreply.github.com>
2019-01-16 09:48:09 -0600

    did not know pep8 was required for this file

--- database_setup.py
+++ database_setup.py
@@ -70,31 +70,31 @@ class User(Base):
         return pwd_context.verify(password, self.password_hash)
 
     def generate_auth_token(self, expiration=600):
-    	s = Serializer(secret_key, expires_in = expiration)
-    	return s.dumps({'id': self.id })
+        s = Serializer(secret_key, expires_in=expiration)
+        return s.dumps({'id': self.id})
 
     @staticmethod
     def verify_auth_token(token):
-    	s = Serializer(secret_key)
-    	try:
-    		data = s.loads(token)
-    	except SignatureExpired:
-    		#Valid Token, but expired
-    		return None
-    	except BadSignature:
-    		#Invalid Token
-    		return None
-    	user_id = data['id']
-    	return user_id
+        s = Serializer(secret_key)
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            # Valid Token, but expired
+            return None
+        except BadSignature:
+            # Invalid Token
+            return None
+        user_id = data['id']
+        return user_id
 
 
-#engine = create_engine('sqlite:///jobtools.db')
+# engine = create_engine('sqlite:///jobtools.db')
 engine = create_engine('postgresql://catalog:Passw0rd@localhost:5432/catalog')
 
 
 Base.metadata.create_all(engine)
 
-#data
+# data
 if __name__ == '__main__':
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
@@ -132,46 +132,126 @@ if __name__ == '__main__':
     session.commit()
 
     job = session.query(Job).filter_by(name='Electrician').one()
-    newTool = Tool(name='Wire Cutters', job_id = job.id, description='A tool that cuts wires.  It usually has features allowing the stripping and trimming of wires as well.', owner='rondevwork@gmail.com')
+    desc = 'A tool that cuts wires.  It usually has features '
+    desc += 'allowing the stripping and trimming of wires as well.'
+    newTool = Tool(name='Wire Cutters',
+                   job_id=job.id,
+                   description=desc,
+                   owner='rondevwork@gmail.com')
     session.add(newTool)
-    newTool = Tool(name='Meter', job_id = job.id, description='This tool allows the measuring of voltages and amps in a given circuit.', owner='rondevwork@gmail.com')
+    desc = 'This tool allows the measuring of voltages '
+    desc += 'and amps in a given circuit.'
+    newTool = Tool(name='Meter',
+                   job_id=job.id,
+                   description=desc,
+                   owner='rondevwork@gmail.com')
     session.add(newTool)
+
     job = session.query(Job).filter_by(name='Plumber').one()
-    newTool = Tool(name='Wrench', job_id = job.id, description='A tool that can be used to loosen or tighten nuts or bolts or any threaded connector.', owner='rondevwork@gmail.com')
+    desc = 'A tool that can be used to loosen or tighten nuts or '
+    desc += 'bolts or any threaded connector.'
+    newTool = Tool(name='Wrench',
+                   job_id=job.id,
+                   description=desc,
+                   owner='rondevwork@gmail.com')
     session.add(newTool)
-    newTool = Tool(name='Plumbers Tape', job_id = job.id, description='Tape used to make threaded connections water tight.', owner='rondevwork@gmail.com')
+    desc = 'Tape used to make threaded connections water tight.'
+    newTool = Tool(name='Plumbers Tape',
+                   job_id=job.id,
+                   description=desc,
+                   owner='rondevwork@gmail.com')
     session.add(newTool)
+
     job = session.query(Job).filter_by(name='Carpenter').one()
-    newTool = Tool(name='Hammer', job_id = job.id, description='A tool for defeating Thanos.', owner='rondevwork@gmail.com')
+    desc = 'A tool for defeating Thanos.'
+    newTool = Tool(name='Hammer',
+                   job_id=job.id,
+                   description=desc,
+                   owner='rondevwork@gmail.com')
     session.add(newTool)
-    newTool = Tool(name='Measuring Tape', job_id = job.id, description='A tool used to measure distances.  Often used to measure lengths for boards used to build things.', owner='rondevwork@gmail.com')
+    desc = 'A tool used to measure distances.  Often used to measure '
+    desc += 'lengths for boards used to build things.'
+    newTool = Tool(name='Measuring Tape',
+                   job_id=job.id,
+                   description=desc,
+                   owner='rondevwork@gmail.com')
     session.add(newTool)
+
     job = session.query(Job).filter_by(name='Mason').one()
-    newTool = Tool(name='Cement Mixer', job_id = job.id, description='A device used to mix the ingredients of cement together.', owner='rondevwork@gmail.com')
+    desc = 'A device used to mix the ingredients of cement together.'
+    newTool = Tool(name='Cement Mixer',
+                   job_id=job.id,
+                   description=desc,
+                   owner='rondevwork@gmail.com')
     session.add(newTool)
-    newTool = Tool(name='Trowel', job_id = job.id, description='A tool that spreads mortar for bricks or tile.', owner='rondevwork@gmail.com')
+    desc = 'A tool that spreads mortar for bricks or tile.'
+    newTool = Tool(name='Trowel',
+                   job_id=job.id,
+                   description=desc,
+                   owner='rondevwork@gmail.com')
     session.add(newTool)
+
     job = session.query(Job).filter_by(name='Roofer').one()
-    newTool = Tool(name='Hammer', job_id = job.id, description='A tool for defeating Thanos but used by a roofer.', owner='rondevwork@gmail.com')
+    desc = 'A tool for defeating Thanos but used by a roofer.'
+    newTool = Tool(name='Hammer',
+                   job_id=job.id,
+                   description=desc,
+                   owner='rondevwork@gmail.com')
     session.add(newTool)
-    newTool = Tool(name='Ladder', job_id = job.id, description='A tool allows the user to climb to altitudes unreachable by other means.', owner='rondevwork@gmail.com')
+    desc = 'A tool allows the user to climb to altitudes unreachable '
+    desc += 'by other means.'
+    newTool = Tool(name='Ladder',
+                   job_id=job.id,
+                   description=desc,
+                   owner='rondevwork@gmail.com')
     session.add(newTool)
+
     job = session.query(Job).filter_by(name='Gardener').one()
-    newTool = Tool(name='Hoe', job_id = job.id, description='A tool for digging in the dirt in a line.', owner='rondevwork@gmail.com')
+    desc = 'A tool for digging in the dirt in a line.'
+    newTool = Tool(name='Hoe',
+                   job_id=job.id,
+                   description=desc,
+                   owner='rondevwork@gmail.com')
     session.add(newTool)
-    newTool = Tool(name='Shovel', job_id = job.id, description='A tool for digging in the dirt.', owner='rlewis8@gmail.com')
+    desc = 'A tool for digging in the dirt.'
+    newTool = Tool(name='Shovel',
+                   job_id=job.id,
+                   description=desc,
+                   owner='rlewis8@gmail.com')
     session.add(newTool)
+
     job = session.query(Job).filter_by(name='Landscaper').one()
-    newTool = Tool(name='Shovel', job_id = job.id, description='A tool for digging in the dirt.', owner='rlewis8@gmail.com')
+    desc = 'A tool for digging in the dirt.'
+    newTool = Tool(name='Shovel',
+                   job_id=job.id,
+                   description=desc,
+                   owner='rlewis8@gmail.com')
     session.add(newTool)
-    newTool = Tool(name='Lawnmower', job_id = job.id, description='A tool that cuts grass', owner='rlewis8@gmail.com')
+    desc = 'A tool that cuts grass.'
+    newTool = Tool(name='Lawnmower',
+                   job_id=job.id,
+                   description=desc,
+                   owner='rlewis8@gmail.com')
     session.add(newTool)
-    newTool = Tool(name='Weedeater', job_id = job.id, description='A tool that cuts weeds in places a lawnmower cannot reach', owner='rlewis8@gmail.com')
+    desc = 'A tool that cuts weeds in places a lawnmower cannot reach.'
+    newTool = Tool(name='Weedeater',
+                   job_id=job.id,
+                   description=desc,
+                   owner='rlewis8@gmail.com')
     session.add(newTool)
+
     job = session.query(Job).filter_by(name='Welder').one()
-    newTool = Tool(name='Mask', job_id = job.id, description='A device that covers the face for protection during welding.', owner='rlewis8@gmail.com')
+    desc = 'A device that covers the face for protection during welding.'
+    newTool = Tool(name='Mask',
+                   job_id=job.id,
+                   description=desc,
+                   owner='rlewis8@gmail.com')
     session.add(newTool)
-    newTool = Tool(name='Torch', job_id = job.id, description='A tool that melts metal.', owner='rlewis8@gmail.com')
+    desc = 'A tool that melts metal.'
+    newTool = Tool(name='Torch',
+                   job_id=job.id,
+                   description=desc,
+                   owner='rlewis8@gmail.com')
     session.add(newTool)
 
     session.commit()
